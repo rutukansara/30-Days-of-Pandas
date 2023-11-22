94416daace438ad1bbfc4e0b09ad6f77b18159df
Rutu Kansara <149906777+rutukansara@users.noreply.github.com>
2023-11-22 13:25:54 -0800

    Time: 1179 ms (15.98%), Space: 0B (100.00%) - LeetHub

--- 2356-number-of-unique-subjects-taught-by-each-teacher/2356-number-of-unique-subjects-taught-by-each-teacher.sql
+++ 2356-number-of-unique-subjects-taught-by-each-teacher/2356-number-of-unique-subjects-taught-by-each-teacher.sql
@@ -1,3 +1,6 @@
 # Write your MySQL query statement below
-SELECT teacher_id, COUNT(DISTINCT subject_id) AS cnt FROM (SELECT t1.teacher_id, t1.subject_id FROM Teacher t1, Teacher t2 WHERE t1.teacher_id = t2.teacher_id) a
+# SELECT teacher_id, COUNT(DISTINCT subject_id) AS cnt FROM (SELECT t1.teacher_id, t1.subject_id FROM Teacher t1, Teacher t2 WHERE t1.teacher_id = t2.teacher_id) a
+# GROUP BY teacher_id
+
+SELECT teacher_id, COUNT(DISTINCT subject_id) AS cnt FROM Teacher
 GROUP BY teacher_id
